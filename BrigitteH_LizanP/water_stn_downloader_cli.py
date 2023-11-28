59d7d313b83e5548671e732f47fcb22148bdf7f3
Brigitte Hanna <hann0327@algonquinlive.com>
2023-11-28 14:56:19 -0500

    Step 3-4

--- BrigitteH_LizanP/water_stn_downloader_cli.py
+++ BrigitteH_LizanP/water_stn_downloader_cli.py
@@ -25,9 +25,12 @@ def main():
     # If no file is specified, print 
     # Usage:  water_stn_downloader_cli.py <out_json_file>
     # and exit the script (i.e. sys.exit(0))
+    if len(sys.argv) < 2:
+        print('Usage:  water_stn_downloader_cli.py <out_json_file>')
+        sys.exit(0)
 
     # TODO: set wsd.out_json_filename to the command-line argument
-    wsd.out_json_filename = ''
+    wsd.out_json_filename = sys.argv[1]
 
     # TODO: Set the wsd.url to the RESTful API endpoint.  
     # That is http:// to the last character before the parameters 
@@ -35,24 +38,27 @@ def main():
     # For long urls, use string concatenation or line continuation to limit
     # statement length to 80 char or less
     #
-    wsd.url = ''
+    wsd.url = 'https://maps-cartes.ec.gc.ca/arcgis/rest/services/CESI_FGP_All_Layers/MapServer/6/query'
 
     # TODO: Set the wsd.params argument where
     # key = name of the string query parameter and
     # value = content assigned to the query parameter
     #
-    wsd.params = {}
+    wsd.params = {'f':'json', 'where': 'OBJECTID>0', 'outFields': '*'}
 
     # TODO: Call wsd.download_to_file()
     #       and set the return value to a variable named status
     #
-    status = ''
+    status = wsd.download_to_file()
 
     # TODO:  Print a message to inform the user what as happened
     # Based on the value of status, if the file downloaded successfully,
     # print "Download successful to wsd.out_json_filename"
     # If the file did not download successfully, print the status value
-
+    if status == 200:
+        print("Download successful to wsd.out_json_filename")
+    else:
+        print(status)
 
 if __name__ == '__main__':
     main()
